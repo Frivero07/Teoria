53ecad1deb8da9efd8c17de2a3f7f394951f26b9
Frivero07 <141739171+Frivero07@users.noreply.github.com>
2024-11-01 17:15:58 -0300

    Add files via upload

---
+++ DI/Program.cs
@@ -0,0 +1,47 @@
+﻿
+//patron de diseño INYECCION DE DEPENDECIA (DI)
+public interface INotificador
+{
+    void Enviar(string mensaje);
+
+}
+
+public class NotificadorSMS : INotificador
+{
+     public void Enviar(string mensaje)
+     {
+        Console.WriteLine($"Enviando SMS: {mensaje}");
+     }
+}
+
+public class NotificadorEmail : INotificador
+{
+    public void Enviar( string mensaje)
+    {
+       Console.WriteLine($"Enviando Email: {mensaje}");
+    }
+}
+
+public class ServicioNotificaciones
+{
+    //se llama al notificador pero sin saber cual y se puede usar cualquiera, cambio de dependencia raido
+    private readonly INotificador _notificador;
+    public ServicioNotificaciones(INotificador notificador)
+    {
+        _notificador = notificador;
+    }
+
+    public void  NotificarUsuario(string mensaje)
+    {
+        _notificador.Enviar(mensaje);
+    }
+}
+public class Program
+{
+    static void Main()
+    {
+        INotificador notificador = new NotificadorSMS();
+        ServicioNotificaciones servicioNotificaciones = new ServicioNotificaciones(notificador);
+        servicioNotificaciones.NotificarUsuario("Hola");
+    }
+}
